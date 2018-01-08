4f214bfd4fc5fcbb302ce116e10ddd09a687407e
anandh51295 <anandharaj51295@gmail.com.com>
2018-01-08 15:56:57 +0530

    Deploy

--- src/main/java/com/javasampleapproach/mongodbrestapi/model/User.java
+++ src/main/java/com/javasampleapproach/mongodbrestapi/model/User.java
@@ -1,28 +1,27 @@
 package com.javasampleapproach.mongodbrestapi.model;
 
 import org.springframework.data.annotation.Id;
-
+import org.springframework.data.annotation.PersistenceConstructor;
+import java.util.concurrent.atomic.AtomicLong;
 import java.io.Serializable;
 
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    private String id;
+    private long id;
     private String email;
     private String password;
+
+    private static AtomicLong COUNTER = new AtomicLong(0L);
+
+    @PersistenceConstructor
+    public User() {
+        this.id = COUNTER.incrementAndGet();
+    }
     @Override
     public String toString() {
         return String.format("Users[id=%s, email='%s', password='%s']", id, email, password);
     }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
 
 
     public String getEmail() {
