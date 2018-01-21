56cdf9e8ce2fd2f28da7f7a370a65d0aba7c7656
anandh51295 <anandharaj51295@gmail.com.com>
2018-01-21 13:48:00 +0530

    Deploy

--- src/main/java/com/javasampleapproach/mongodbrestapi/controller/UController.java
+++ src/main/java/com/javasampleapproach/mongodbrestapi/controller/UController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import com.javasampleapproach.mongodbrestapi.model.User;
 import com.javasampleapproach.mongodbrestapi.repository.UserRepository;
 
-import javax.servlet.http.HttpServletResponse;
-
 @RestController
 @RequestMapping("/api/auth")
 public class UController {
@@ -16,25 +14,23 @@ public class UController {
     UserRepository userRepository;
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void create(@RequestBody User user, HttpServletResponse response){
-        response.setStatus(HttpServletResponse.SC_ACCEPTED);
+    public void create(@RequestBody User user){
         userRepository.save(user);
     }
 
     @RequestMapping(value = "/{id}")
-    public User read(@PathVariable String id, HttpServletResponse response){
-        response.setStatus(HttpServletResponse.SC_ACCEPTED);
+    public User read(@PathVariable String id){
+
         return  userRepository.findOne(id);
     }
 
+
     @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void update(@RequestBody User user, HttpServletResponse response){
-        response.setStatus(HttpServletResponse.SC_ACCEPTED);
+    public void update(@RequestBody User user){
         userRepository.save(user);
     }
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable String id, HttpServletResponse response) {
-        response.setStatus(HttpServletResponse.SC_ACCEPTED);
+    public void delete(@PathVariable String id) {
         userRepository.delete(id);
     }
 
